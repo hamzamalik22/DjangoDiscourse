750aca16cf191e8ee5ebb4b3b744b07b790d9b8f
hamzamalik22 <hamzamalikaj22@gmail.com>
2023-11-23 21:45:24 +0500

    CRUD operations on Chat Rooms

--- DjangoDiscourse/chat/views.py
+++ DjangoDiscourse/chat/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
+from .forms import *
 
 
 
@@ -21,3 +22,35 @@ def room(request,pk):
     room = Room.objects.get(id=pk)
     context = {'room' : room}
     return render(request,'chat/room.html',context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method ==  'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form' : form}
+    return render(request,'chat/room_form.html',context)
+
+
+def updateRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form' : form}
+    return render(request,'chat/room_form.html',context)
+
+def deleteRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    context = {'obj' : room}
+    return render(request,'chat/delete.html',context)
