2fc79d6c9e215bab921bcca5a7d68d43d322419a
hamzamalik22 <hamzamalikaj22@gmail.com>
2023-11-23 13:45:32 +0500

    configure urls,views and templates

--- DjangoDiscourse/chat/views.py
+++ DjangoDiscourse/chat/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+rooms = [
+    {'id' : 1 , 'name' : "Python here & there"},
+    {'id' : 2 , 'name' : "movies i can watch"},
+    {'id' : 3 , 'name' : "anime lovers"},
+]
+
+
+
+def home(request):
+    context = {'rooms' : rooms}
+    return render(request,'chat/home.html',context)
+
+
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room' : room,'page' : f'{i["name"]}'.upper()}
+    return render(request,'chat/room.html',context)
