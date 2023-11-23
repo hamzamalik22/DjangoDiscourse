9b495a8a4c6f46a7e897a2c63f5017181530c27f
hamzamalik22 <hamzamalikaj22@gmail.com>
2023-11-23 17:34:21 +0500

    define the models of app

--- DjangoDiscourse/chat/views.py
+++ DjangoDiscourse/chat/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render
+from .models import *
 
 
 
-rooms = [
-    {'id' : 1 , 'name' : "Python here & there"},
-    {'id' : 2 , 'name' : "movies i can watch"},
-    {'id' : 3 , 'name' : "anime lovers"},
-]
+# rooms = [
+#     {'id' : 1 , 'name' : "Python here & there"},
+#     {'id' : 2 , 'name' : "movies i can watch"},
+#     {'id' : 3 , 'name' : "anime lovers"},
+# ]
 
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms' : rooms}
     return render(request,'chat/home.html',context)
 
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-    context = {'room' : room,'page' : f'{i["name"]}'.upper()}
+    room = Room.objects.get(id=pk)
+    context = {'room' : room}
     return render(request,'chat/room.html',context)
