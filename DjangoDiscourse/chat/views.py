8cb4293aa5b63c804426abc90853bb539316d55c
hamzamalik22 <hamzamalikaj22@gmail.com>
2023-11-30 00:05:36 +0500

    Added GUI to Creation/ Update form

--- DjangoDiscourse/chat/views.py
+++ DjangoDiscourse/chat/views.py
@@ -57,14 +57,28 @@ def room(request,pk):
 @login_required(login_url='loginpage')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method ==  'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
-    context = {'form' : form,'page' : 'Create Room'}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name = topic_name)
+
+        Room.objects.create(
+            host = request.user,
+            topic = topic ,
+            name = request.POST.get('name'),
+            description = request.POST.get('description'),
+
+        )
+
+
+        # form = RoomForm(request.POST)
+        # if form.is_valid():
+        #     room = form.save(commit=False)
+        #     room.host = request.user
+        #     room.save()
+
+        return redirect('home')
+    context = {'form' : form,'page' : 'Create Room','topics' : topics}
     return render(request,'chat/room_form.html',context)
 
 
@@ -72,18 +86,22 @@ def createRoom(request):
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if  request.user != room.host:
         messages.error(request,"Only Room Owner can edit!")
         return redirect('home')
 
     if request.method == 'POST':
-        form = RoomForm(request.POST,instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name = topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
 
-    context = {'form' : form,'page' : 'Update Room'}
+    context = {'form' : form,'page' : 'Update Room','topics' : topics,'room' : room}
     return render(request,'chat/room_form.html',context)
 
 
