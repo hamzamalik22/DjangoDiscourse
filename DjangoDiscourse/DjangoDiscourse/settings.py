4d20aebce2f5fc83d5fb1517baf916aa0574db02
hamzamalik22 <hamzamalikaj22@gmail.com>
2023-12-05 00:36:23 +0500

    Created route for api

--- DjangoDiscourse/DjangoDiscourse/settings.py
+++ DjangoDiscourse/DjangoDiscourse/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
     'chat.apps.ChatConfig',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
